1442945c8f3c20fb6a0e8452b74af3f8c07097e2
Alex Zakharov <zakharov61@yandex.ru>
2024-03-11 19:35:48 +0300

    Add file

---
+++ src/test/java/ru/netology/ShopRepositoryTest.java
@@ -0,0 +1,58 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+
+    ShopRepository repo = new ShopRepository();
+
+
+    Product product1 = new Product(1, "product1", 1990);
+    Product product2 = new Product(2, "product2", 399);
+    Product product3 = new Product(3, "product3", 69);
+
+    @Test
+    public void shouldAddProductInRepository() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Product[] expected = {product1, product2, product3};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDeleteProductInRepository() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.removeById(1);
+
+        Product[] expected = {product2, product3};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(77);
+        });
+    }
+    @Test
+    public void shouldExceptionRemoveById() {
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(15);
+        });
+    }
+
+}
